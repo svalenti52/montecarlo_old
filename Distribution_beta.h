f6acb2cbf8b63f3f0399fdb6d4937bb756e7dac5
svalenti52 <svalenti52@yahoo.com>
2017-09-02 11:56:34 -0700

    Added new distributions: normal distribution and piece-wise constant distribution. Just comments and renaming were needed for the normal distribution.

--- Distribution_beta.h
+++ Distribution_beta.h
@@ -23,17 +23,18 @@ class Distribution {
 public:
 
     /**
-     * Constructor used for uniform integer and real distributions
+     * Constructor used for uniform integer and real distributions, also the
+     * normal distribution uses it with mean and standard deviation.
      * @param _min Minimum value to be used by the random number distribution
      * @param _max Maximum value to be used by the random number distribution
      * @param _nr_events Number of sequential events to generated
      */
     Distribution(
-            X_AXIS _min,
-            X_AXIS _max,
+            X_AXIS _min_or_mean,
+            X_AXIS _max_or_stddev,
             int _nr_events)
 
-            : randomDistribution(_min, _max), nr_events(_nr_events) {}
+            : randomDistribution(_min_or_mean, _max_or_stddev), nr_events(_nr_events) {}
 
     /**
      * Constructor used for Bernoulli, Poisson, Exponential distributions
@@ -48,7 +49,7 @@ public:
 
     using ITERATOR = std::vector<double>::iterator;
     /**
-     * Constructor used for Piece-wise Linear distribution
+     * Constructor used for the Piece-wise Linear distribution
      * @param _begin_intervals begin iterator for vector of intervals
      * @param _end_intervals end iterator for vector of intervals
      * @param _begin_weights begin iterator for vector of weights (probabilities)
@@ -63,6 +64,24 @@ public:
             : randomDistribution(_begin_intervals, _end_intervals, _begin_weights),
             nr_events(_nr_events) {}
 
+    /**
+     * Constructor for the Piece-wise Constant distribution
+     * @param _begin_intervals begin iterator for vector of intervals
+     * @param _end_intervals end iterator for vector of intervals
+     * @param _begin_weights begin iterator for vector of weights (probabilities)
+     * @param _end_weights weights vector is one less than the interval vector (only need for left edge)
+     * @param _nr_events
+     */
+    Distribution(
+            ITERATOR _begin_intervals,
+            ITERATOR _end_intervals,
+            ITERATOR _begin_weights,
+            ITERATOR _end_weights,
+            int _nr_events)
+
+            : randomDistribution(_begin_intervals, _end_intervals, _begin_weights, _end_weights),
+              nr_events(_nr_events) {}
+    //-------------------------------------------------------------------------
     /**
      *
      * @param dre
