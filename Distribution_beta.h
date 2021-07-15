cfbadda1ecacd16cff57823caaefa974dbed2be6
svalenti52 <svalenti52@yahoo.com>
2021-07-15 09:03:23 -0700

    Changed to C++20. Added Distribution_NTT and MonteCarloSimulation_NTT to accommodate a non-template-template class as needed by Bernoulli distribution.

--- Distribution_beta.h
+++ Distribution_beta.h
@@ -148,4 +148,136 @@ public:
     std::deque<X_AXIS> events;
 };
 
+template <class X_AXIS, class PARAM, class RANDOM_DIST >
+class Distribution_NTT {
+    RANDOM_DIST randomDistribution;
+    int nr_events;
+public:
+
+    /**
+     * Constructor used for uniform integer and real distributions, also the
+     * normal distribution uses it with mean and standard deviation.
+     * @param _min Minimum value to be used by the random number distribution
+     * @param _max Maximum value to be used by the random number distribution
+     * @param _nr_events Number of sequential events to generated
+     */
+    Distribution_NTT(
+            X_AXIS _min_or_mean,
+            X_AXIS _max_or_stddev,
+            int _nr_events)
+
+            : randomDistribution(_min_or_mean, _max_or_stddev), nr_events(_nr_events) {}
+
+    /**
+     * Constructor used for Bernoulli, Poisson, Exponential distributions
+     * @param _likelihood (probability_of_true, mean, lambda for above distributions)
+     * @param _nr_events Number of sequential events to generated
+     */
+    Distribution_NTT(
+            PARAM _likelihood,
+            int _nr_events)
+
+            : randomDistribution(_likelihood), nr_events(_nr_events) {}
+
+    using ITERATOR = std::vector<double>::iterator;
+    /**
+     * Constructor used for the Piece-wise Linear distribution
+     * @param _begin_intervals begin iterator for vector of intervals
+     * @param _end_intervals end iterator for vector of intervals
+     * @param _begin_weights begin iterator for vector of weights (probabilities)
+     * @param _nr_events Number of sequential events to generated
+     */
+    Distribution_NTT(
+            ITERATOR _begin_intervals,
+            ITERATOR _end_intervals,
+            ITERATOR _begin_weights,
+            int _nr_events)
+
+            : randomDistribution(_begin_intervals, _end_intervals, _begin_weights),
+              nr_events(_nr_events) {}
+
+    /**
+     * Constructor for the Piece-wise Constant distribution
+     * @param _begin_intervals begin iterator for vector of intervals
+     * @param _end_intervals end iterator for vector of intervals
+     * @param _begin_weights begin iterator for vector of weights (probabilities)
+     * @param _end_weights weights vector is one less than the interval vector (only need for left edge)
+     * @param _nr_events
+     */
+    Distribution_NTT(
+            ITERATOR _begin_intervals,
+            ITERATOR _end_intervals,
+            ITERATOR _begin_weights,
+            ITERATOR _end_weights,
+            int _nr_events)
+
+            : randomDistribution(_begin_intervals, _end_intervals, _begin_weights, _end_weights),
+              nr_events(_nr_events) {}
+    //-------------------------------------------------------------------------
+    /**
+     *
+     * @param dre
+     */
+    void load_random_values(std::default_random_engine& dre) {
+        for ( int ix = 0; ix < nr_events; ++ix )
+            events.push_back(randomDistribution(dre));
+    }
+
+    /**
+     *
+     * @param dre
+     */
+    void reload_random_values(std::default_random_engine& dre) {
+        for ( X_AXIS& value : events )
+            value = randomDistribution(dre);
+    }
+
+    /**
+     *
+     * @param vector_of_values
+     */
+    void reload_values (const std::vector<X_AXIS>& vector_of_values) {
+        for ( int ix = 0; ix < events.size(); ++ix )
+            events[ix] = vector_of_values[ix];
+    }
+
+    /**
+     *
+     * @param index
+     * @param dre
+     */
+    void reload_random_value(int index, std::default_random_engine& dre) {
+        events[index] = randomDistribution(dre);
+    }
+
+    /**
+     *
+     * @param dre
+     */
+    void add_random_value_to_end(std::default_random_engine& dre) {
+        events.push_back(randomDistribution(dre));
+    }
+
+    /**
+     *
+     * @return
+     */
+    X_AXIS sum() {
+        X_AXIS cumulative = 0;
+        for ( X_AXIS element : events )
+            cumulative += element;
+        return cumulative;
+    }
+
+    /**
+    * show_contents - show, on cout, the contents of the deque
+    */
+    void show_contents() {
+        for ( X_AXIS event : events )
+            std::cout << event << "  ";
+    }
+
+    std::deque<X_AXIS> events;
+};
+
 #endif //MONTECARLO_DISTRIBUTION_BETA_H
--- MonteCarloSim_beta.h
+++ MonteCarloSim_beta.h
@@ -93,4 +93,80 @@ public:
     }
 };
 
+/**
+ * MonteCarloSimulation Class, consists of Constructor and the methods: run, change_message, and
+ * print_result. This is a reworking of the original MonteCarloSim class. The main differences are the
+ * presence of a random number generator and a template for the y-axis value.
+ * @tparam X_AXIS - For the x-axis or domain of the distribution, expected to be either integral or floating point.
+ * @tparam Y_AXIS - For the y-axis or range of the distribution, expected to be either integral or floating point.
+ * @tparam PARAM - Input parameter type, e.g, for Poisson it is of real type even though values T are integral
+ * @tparam STD_DIST - A template template of the Distribution - when object created, e.g., std::uniform_int_distribution
+ */
+
+template <class X_AXIS, class Y_AXIS, class PARAM, class STD_DIST>
+class MonteCarloSimulation_NTT {
+protected:
+    int nr_trials; ///> number of repeated trials run for the simulation
+    DRE dre;			///> default random engine
+    Y_AXIS cumulative_value; ///> accumulates the interim value for nr_trials
+    Y_AXIS interim_value;		///> value determined for each trial
+    std::string message; ///> message can be changed to match the meaning of cumulative_value/nr_trials
+    Distribution_NTT<X_AXIS, PARAM, STD_DIST> distribution; ///> (e.g., real) and deque of numbers selected from it
+    std::function<bool(Distribution_NTT<X_AXIS, PARAM, STD_DIST>&, Y_AXIS&, DRE&)> condition_met; ///> function containing particulars of the simulation
+
+public:
+
+    /**
+     * MonteCarloSimulation - latest attempt at generalising a system includes separating the
+     * random number generator from the distribution. Also includes establishing a type for the
+     * values (class U for the y-axis).
+     * @param _nr_trials number of repeated trials run for the simulation
+     * @param _seed integer seed for the random number engine
+     * @param _condition_met function containing particulars of the simulation
+     * @param _distribution (e.g., real) and deque of numbers selected from it
+     */
+    MonteCarloSimulation_NTT ( int _nr_trials, int _seed,
+                           std::function<bool(Distribution_NTT<X_AXIS, PARAM, STD_DIST>&, Y_AXIS&, DRE&)> _condition_met,
+                           Distribution_NTT<X_AXIS, PARAM, STD_DIST>& _distribution )
+            : nr_trials(_nr_trials), dre(_seed),
+              cumulative_value(0), interim_value(1),
+              message("probability is = "),
+              condition_met(_condition_met),
+              distribution(std::move(_distribution))
+    {
+        distribution.load_random_values(dre);
+    }
+
+    /**
+     * The signatures of the "reload_random_values" and "condition_met"
+     * functions were changed to accept dre from here.
+     */
+    virtual void run() {
+        for ( int ix = 0; ix < nr_trials; ++ix ) {
+            if ( condition_met(distribution, interim_value, dre) )
+                cumulative_value += interim_value;
+            distribution.reload_random_values(dre);
+        }
+    }
+
+    virtual void change_message(const std::string& s) {
+        message = s;
+    }
+
+    virtual void print_result() {
+        std::cout << message
+                  << static_cast<double>(cumulative_value)
+                     /
+                     static_cast<double>(nr_trials) << '\n';
+    }
+
+    /**
+     * show_distribution - shows the contents of the distribution
+     */
+    void show_distribution() {
+        distribution.show_contents();
+        std::cout << '\n';
+    }
+};
+
 #endif //MONTECARLO_MONTECARLOSIM_BETA_H
